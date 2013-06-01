d4f1c687207ab082202859a4d1c80c598023e5e3
jdaley <joe@fastmail.com.au>
2013-06-01 10:48:49 +1000

    Create blank MVC project

---
+++ SydneyLexicographer/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SydneyLexicographer
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SydneyLexicographer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SydneyLexicographer.MvcApplication" Language="C#" %>
