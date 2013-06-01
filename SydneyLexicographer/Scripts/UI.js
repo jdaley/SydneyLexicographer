c44ff7d7a9d4dac1de7fd6ca75fe40fd6b07dafd
jdaley <joe@fastmail.com.au>
2013-06-01 15:04:11 +1000

    Some visuals and CSS

---
+++ SydneyLexicographer/Scripts/UI.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#yearSlider").slider({
+        min: 1777,
+        max: 2013,
+        values: [1777],
+        slide: function (event, ui) {
+            $("#date").val(ui.values[0]);
+        }
+    });
+
+    $("#date").val($("#yearSlider").slider("values", 0));
+});
