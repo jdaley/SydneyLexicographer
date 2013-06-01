f2726cfa4974fb7655bf806b96f064c9f42ae50c
Kate Dreaper <kate.dreaper@gmx.com>
2013-06-01 17:24:49 +1000

    Add correct answer to year slider

--- SydneyLexicographer/Scripts/App.js
+++ SydneyLexicographer/Scripts/App.js
@@ -1,6 +1,7 @@
 ﻿var MAX_YEAR = 2013;
 var MIN_YEAR = 1777;
 var MAX_DISTANCE = 60;
+var MAX_QUESTIONS = 10;
 
 var gMaps = {
     answerLatitude: '',
@@ -11,9 +12,27 @@ var answer = {};
 SL.runningScore = 0;
 SL.questionNumber = 1;
 
-SL.calculateYearScore = function (question) {
+SL.calculateYearScore = function () {
     var yearGuess = $("#date").val();
-    return 100 - Math.round((Math.abs(question.Year - yearGuess) / (MAX_YEAR - MIN_YEAR)) * 100);
+    return 100 - Math.round((Math.abs(answer.Year - yearGuess) / (MAX_YEAR - MIN_YEAR)) * 100);
+};
+
+SL.displayYearAnswer = function (question) {
+    var yearGuess = $("#date").val();
+    if (yearGuess == answer.Year) {
+        $(".question-timeline-content").addClass("correct");
+    }
+    else{
+        $("#yearSlider").slider({"values": [yearGuess,answer.Year],"range": "true"});
+    }
+    $("#correct-answer").text(answer.Year);
+    $("#correct-answer-container").show();
+};
+
+SL.resetYear = function(){
+    $("#yearSlider").slider({ "values": [MIN_YEAR], "range": "false" });
+    $(".question-timeline-content").removeClass("correct");
+    $("#correct-answer-container").hide();
 };
 
 SL.calculateMapScore = function(question) {
@@ -35,8 +54,14 @@ SL.loadQuestion = function() {
         $("#question-number").text(SL.questionNumber);
         $("#timeline-score").hide();
         $("#map-score").hide();
+        SL.resetYear();
     });
 }
+
+SL.updateTotalScore = function () {
+    $("#score").text(SL.runningScore);
+    $("#score-out-of").text((SL.questionNumber - 1) * 100);
+};
 
 $(function () {
     $("#submitButton").hide();
@@ -51,17 +76,35 @@ $(function () {
         SL.runningScore = SL.runningScore + totalScore;
         SL.questionNumber = SL.questionNumber + 1;
         var mapScore = 0;
+        showAnswer();
+        SL.displayYearAnswer();
+        SL.updateTotalScore();
         $("#timeline-score").text(yearScore);
         $("#map-score").text(mapScore);
-        $("#score").text(SL.runningScore);
-        $("#score-out-of").text((SL.questionNumber-1)*100);
         $('#name').text(answer.Name);
         $('#description').text(answer.Description);
-        showAnswer();
         $("#timeline-score").show();
         $("#map-score").show();
         $("#nextQuestionButton").show();
         $("#submitButton").hide();
+
+        if (SL.questionNumber > MAX_QUESTIONS) {
+            $("#results").text(SL.runningScore);
+            $("#results").dialog({
+                modal: true,
+                title: "Final Score",
+                buttons: {
+                    "Start Again" : function () {
+                        $(this).dialog("close");
+                        SL.runningScore = 0;
+                        SL.questionNumber = 1;
+                        SL.updateTotalScore();
+                        SL.loadQuestion();
+                    }
+                }
+            });
+        }
     });
+
     SL.loadQuestion();
 });
