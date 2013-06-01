6286287eaec2223e9f5be3e5334721f14695f20d
Kate Dreaper <kate.dreaper@gmx.com>
2013-06-01 12:26:32 +1000

    Calculate year score

---
+++ SydneyLexicographer/Scripts/App.js
@@ -0,0 +1,29 @@
+﻿var MAX_YEAR = 2013;
+var MIN_YEAR = 1777;
+
+function loadQuestion() {
+    $.ajax({
+        url: '@Url.Content("~/api/Question")',
+        accepts: 'application/json'
+    }).done(function (question) {
+        $('#photo').attr('src', question.PhotoUrl);
+        $('#name').text(question.Name);
+        $('#description').text(question.Description);
+        $("#submitButton").click(function () {
+            var yearGuess = 2000;
+            var yearScore = Math.round(Math.abs(question.Year - yearGuess) / (MAX_YEAR - MIN_YEAR) * 100);
+            $("#score").text(yearScore);
+            $("#nextQuestionButton").show();
+            $("#submitButton").hide();
+        });
+    });
+}
+
+$(function () {
+    $("#submitButton").hide();
+    $("#nextQuestionButton").click(function () {
+        $("#submitButton").show();
+        $("#nextQuestionButton").hide();
+        loadQuestion();
+    });
+});
