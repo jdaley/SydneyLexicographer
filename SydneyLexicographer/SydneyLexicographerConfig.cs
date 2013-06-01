866d2881f0d091cfded0d2c29d7e82a7e28e6e88
jdaley <joe@fastmail.com.au>
2013-06-01 12:07:03 +1000

    Database config file

---
+++ SydneyLexicographer/SydneyLexicographerConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace SydneyLexicographer
+{
+    [XmlRoot("sydneyLexicographer")]
+    public class SydneyLexicographerConfig
+    {
+        public static SydneyLexicographerConfig Current { get; set; }
+
+        static SydneyLexicographerConfig()
+        {
+            string configFileName = "SydneyLexicographer.config";
+            string configPath;
+
+            if (HttpRuntime.AppDomainAppId == null)
+            {
+                configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configFileName);
+            }
+            else
+            {
+                configPath = Path.Combine(HttpRuntime.AppDomainAppPath, configFileName);
+            }
+
+            if (!File.Exists(configPath))
+            {
+                throw new Exception(configFileName + " does not exist.");
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(SydneyLexicographerConfig));
+
+            using (StreamReader reader = new StreamReader(configPath))
+            {
+                Current = (SydneyLexicographerConfig)serializer.Deserialize(reader);
+            }
+        }
+
+        [XmlElement("connectionString")]
+        public string ConnectionString { get; set; }
+    }
+}
