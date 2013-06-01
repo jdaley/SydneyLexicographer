6f71c6ab57e1819fa3498a43d03cde8d54a44469
jdaley <joe@fastmail.com.au>
2013-06-01 11:27:10 +1000

    Question API with dummy data. Example AJAX call to retrieve data.

---
+++ SydneyLexicographer/Controllers/QuestionController.cs
@@ -0,0 +1,28 @@
+﻿using SydneyLexicographer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SydneyLexicographer.Controllers
+{
+    public class QuestionController : ApiController
+    {
+        public Question Get()
+        {
+            return new Question
+            {
+                Id = 54,
+                PhotoUrl = @"http://dictionaryofsydney.org/files/wide/b16fbbf6b053225659d148b51ec14b12b93ba88e",
+                Name = "Court house, Darlinghurst",
+                Description = "Darlinghurst Courthouse was an early purpose-built courthouse that inspired courthouse design throughout the colony for the rest of the nineteenth century. The building redefined the streetscape of Darlinghurst Hill, and played a large part in Sydney's legal history.",
+                Year = 1840,
+                Latitude = 3,
+                Longitude = 3,
+                Difficulty = 1
+            };
+        }
+    }
+}
---
+++ SydneyLexicographer/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SydneyLexicographer.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public int Difficulty { get; set; }
+    }
+}
